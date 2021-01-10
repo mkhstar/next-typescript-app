60db4184eac2beee09d437488fa25a6d604d0707
Kusi Musah Hussein <kusimusah@gmail.com>
2021-01-11 01:31:43 +0300

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import { FC } from 'react';
+import styles from '../styles/Home.module.css';
+
+const Home: FC = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <h1>The Next Typescript App</h1>
+    </div>
+  );
+};
+
+export default Home;
